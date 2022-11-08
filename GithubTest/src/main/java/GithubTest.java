e509ec3abae289dcee87441698b70f34ed574ba3
chitrathiru <116384295+chitrathiru@users.noreply.github.com>
2022-11-08 11:12:58 -0800

    Update GithubTest.java

--- GithubTest/src/main/java/GithubTest.java
+++ GithubTest/src/main/java/GithubTest.java
@@ -4,7 +4,7 @@ public class GithubTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("to test github");
-		System.out.println("to test github push in jenkin second time");
+		System.out.println("to test github push in jenkin firsttime time");
 
 	}
 
