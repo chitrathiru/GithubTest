a3da266a814d021a62bdc7a0b15e818c52391e83
chitrathiru <116384295+chitrathiru@users.noreply.github.com>
2022-11-08 11:10:52 -0800

    Update GithubTest.java
    Commiting after giving credentioal

--- GithubTest/src/main/java/GithubTest.java
+++ GithubTest/src/main/java/GithubTest.java
@@ -4,7 +4,6 @@ public class GithubTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("to test github");
-		// TODO Auto-generated method stub
 		System.out.println("to test github push in jenkin");
 
 	}
